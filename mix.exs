ba494582c5816f839aa4b6451893751f3cfc627b
Thomas Lacroix <toto.rigolo@free.fr>
2021-03-26 01:01:04 +0100

    Use my fork for Xain to fix deprecation warnings

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule ExAdmin.Mixfile do
       {:cowboy, "~> 2.5"},
       {:inflex, "~> 1.7"},
       {:scrivener_ecto, "~> 2.0"},
-      {:xain, "0.6.1"}, #TODO: https://github.com/smpallen99/xain/issues/20
+      {:xain, github: "totorigolo/xain"}, #TODO: https://github.com/smpallen99/xain/issues/20
       {:csvlixir, "~> 1.0.0"},
       {:exactor, "~> 2.2.0"},
       {:ex_doc, "~> 0.22", only: :dev},
